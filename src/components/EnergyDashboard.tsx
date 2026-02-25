fba061323b3663525640e79dcd1ee334b0f4beaf
BLACKPANTHEREH <49166191+mironjee51300@users.noreply.github.com>
2026-02-25 12:01:24 +0530

    Create EnergyDashboard.tsx for real-time energy data visualization

---
+++ src/components/EnergyDashboard.tsx
@@ -0,0 +1,14 @@
+// EnergyDashboard.tsx
+
+import React from 'react';
+
+const EnergyDashboard: React.FC = () => {
+    return (
+        <div>
+            <h1>Real-Time Energy Data Visualization</h1>
+            {/* Add your real-time data visualization components here */}
+        </div>
+    );
+};
+
+export default EnergyDashboard;
